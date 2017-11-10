f16c190e9a863d060abc8768a4de6e1a9a430879
briancarter <briancarter@chipsofttech.com>
2017-11-10 06:13:43 -0500

    First Checkin

---
+++ src/server.js
@@ -0,0 +1,230 @@
+"use strict";
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cli_1 = require("./cli");
+// -------------------- App setup --------------------
+const config = cli_1.getConfig();
+
+function createExpressApp() {
+    let app = express();
+    setupCompression(app);
+    setupJSON(app);
+    if (config.cors)
+        setupCORS(app);
+    return app;
+}
+
+function setupCompression(app) {
+    // Enable gzip compression
+    app.use(compression());
+}
+
+function setupJSON(app) {
+    // Configure app to use bodyParser()
+    // this will let us get the data from a POST
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+}
+
+function setupCORS(app) {
+    app.use(cors());
+}
+
+// -------------------- File handling --------------------
+let files = {};
+let reading = {};
+let changed = {};
+
+function checkDirectorySync(directory) {  
+    try {
+      fs.statSync(directory);
+    } catch(e) {
+      fs.mkdirSync(directory);
+    }
+}
+
+function handleFileRead(fname, success, fail, err, data) {
+    let json = null;
+    let cberr = null;
+    if (err) {
+        // Assume all file read errors mean "file does not exist yet" and create array
+        json = [];
+        files[fname] = json;
+        success(json);
+    }
+    else {
+        try {
+            json = JSON.parse(data);
+            // Update global structure
+            files[fname] = json;
+            // Return JSON object to caller
+            success(json);
+        }
+        catch (jpe) {
+            cberr = { code: 500, msg: 'Error during JSON parse: ' + jpe.toString() };
+            fail(cberr);
+        }
+    }
+    // Notify listeners, if any
+    for (let cb of reading[fname])
+        cb(cberr, json);
+    // File is ready
+    reading[fname] = undefined;
+}
+
+function getJsonFile(fname) {
+    return new Promise((success, fail) => {
+        if (!fname.match(/^[a-zA-Z0-9\.-_]+$/))
+            // Fail at invalid file name
+            return fail({ code: 400, msg: 'Invalid colletion name' });
+        if (files[fname])
+            // File is in memory, just return its content
+            return success(files[fname]);
+        if (reading[fname] !== undefined)
+            // File is being read: subscribe to being called when available
+            return reading[fname].push((err, data) => err ? fail(err) : success(data));
+        // Read the file
+        reading[fname] = [];
+        fs.readFile(config.dataDir + fname + '.json', 'utf8', (err, data) => {
+            handleFileRead(fname, success, fail, err, data);
+        });
+    });
+}
+
+function markChanged(fname) {
+    changed[fname] = true;
+}
+
+function setupPeriodicWrite() {
+    if (!config.writeDelay)
+        return;
+    setInterval(_ => {
+        for (let fname of Object.keys(changed))
+            fs.writeFile(config.dataDir + fname + '.json', JSON.stringify(files[fname], null, 2));
+        changed = {};
+    }, config.writeDelay);
+}
+
+// -------------------- Request handlng --------------------
+function uniqueId(len) {
+    return Math.random().toString(36).substr(2, len);
+}
+
+function reply(res, obj) {
+    const sendReply = () => res.json(obj);
+    if (config.replyDelay)
+        setTimeout(sendReply, config.replyDelay);
+    else
+        sendReply();
+}
+
+function handleError(err, res) {
+    console.log('Error:', err.msg);
+    res.status(err.code);
+    reply(res, { error: err });
+}
+
+function handleGetAll(req, res) {
+    getJsonFile(req.params.file)
+        .then(json => {
+        console.log(`GET for file ${req.params.file}`);
+        reply(res, { msg: 'OK', data: json });
+    })
+        .catch(err => handleError(err, res));
+}
+
+function handleGetOne(req, res) {
+    let id = req.params.id;
+    let fname = req.params.file;
+    getJsonFile(fname)
+        .then(json => {
+        console.log(`GET for file "${fname}", id "${id}"`);
+        let item = json.find(item => item._id == id);
+        if (item) {
+            reply(res, { msg: 'OK', data: item });
+        }
+        else {
+            res.status(404);
+            reply(res, { error: `Item ${id} not found in ${fname}` });
+        }
+    })
+        .catch(err => handleError(err, res));
+}
+
+function handlePost(req, res) {
+    let fname = req.params.file;
+    getJsonFile(fname)
+        .then(json => {
+        console.log(`POST for file ${fname}`);
+        if (!req.body._id)
+            req.body._id = uniqueId(16);
+        json.push(req.body);
+        reply(res, { msg: 'OK', _id: req.body._id });
+        markChanged(fname);
+    })
+        .catch(err => handleError(err, res));
+}
+
+function handlePut(req, res) {
+    let id = req.params.id;
+    let fname = req.params.file;
+    getJsonFile(fname)
+        .then(json => {
+        console.log(`PUT for file "${fname}", id "${id}"`);
+        let idx = json.findIndex(item => item._id == id);
+        if (idx >= 0) {
+            if (!req.body._id)
+                req.body._id = id;
+            json[idx] = req.body;
+            markChanged(fname);
+            reply(res, { msg: 'OK' });
+        }
+        else {
+            res.status(404);
+            reply(res, { error: `Item ${id} not found in ${fname}` });
+        }
+    })
+        .catch(err => handleError(err, res));
+}
+
+function handleDelete(req, res) {
+    let id = req.params.id;
+    let fname = req.params.file;
+    getJsonFile(fname)
+        .then(json => {
+        console.log(`DELETE for file "${fname}", id "${id}"`);
+        let idx = json.findIndex(item => item._id == id);
+        if (idx >= 0) {
+            json.splice(idx, 1);
+            markChanged(fname);
+            reply(res, { msg: 'OK' });
+        }
+        else {
+            res.status(404);
+            reply(res, { error: `Item ${id} not found in ${fname}` });
+        }
+    })
+        .catch(err => handleError(err, res));
+}
+
+// -------------------- Main --------------------
+function main() {
+    let app = createExpressApp();
+    if (config.webRoot)
+        app.use(express.static(config.webRoot));
+    let route = config.apiRoot + '/:file';
+    let routeWithId = route + '/:id';
+    app.get(route, handleGetAll);
+    app.get(routeWithId, handleGetOne);
+    app.post(route, handlePost);
+    app.put(routeWithId, handlePut);
+    app.delete(routeWithId, handleDelete);
+    setupPeriodicWrite();
+    checkDirectorySync(config.dataDir); //directory created / exists, all good.
+    http.createServer(app).listen(config.port);
+}
+main();
